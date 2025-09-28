4461d4431d1060fb66a2a3b0ca5108d36d38896f
Tahani Alhammad <tahani_alhammad@yahoo.com>
2025-09-28 13:09:14 +0200

    update

--- src/components/AppButton.js
+++ src/components/AppButton.js
@@ -8,6 +8,13 @@ export default function AppButton({ href, children, variant = "fill" }) {
     fill: "border-2 border-transparent bg-fuchsia-600 text-white hover:bg-fuchsia-800 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-fuchsia-600",
     outline:
       "border-2 border-fuchsia-600 text-gray-900 hover:bg-fuchsia-500 hover:text-white",
+    success:
+      "border-2 border-transparent bg-green-600 text-white hover:bg-green-700",
+    info: "border-2 border-transparent bg-indigo-600 text-white hover:bg-indigo-700",
+    light: "border-2 border-gray-300 bg-white text-gray-900 hover:bg-gray-100",
+    dark: "border-2 border-transparent bg-gray-900 text-white hover:bg-black",
+    danger:
+      "border-2 border-transparent bg-red-600 text-white hover:bg-red-700",
   };
 
   return (
