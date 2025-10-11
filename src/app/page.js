3af7c40139e134e930ab1674a14b290251627f63
Tahani Alhammad <tahani_alhammad@yahoo.com>
2025-10-11 12:47:24 +0200

    update

--- src/app/page.js
+++ src/app/page.js
@@ -81,7 +81,7 @@ export default function Home() {
             </a>
           </div>
         </main>
-        <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        {/* <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
           <a
             className="flex items-center gap-2 hover:underline hover:underline-offset-4"
             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -127,7 +127,7 @@ export default function Home() {
             />
             Go to nextjs.org →
           </a>
-        </footer>
+        </footer> */}
       </div>
     </>
   );
