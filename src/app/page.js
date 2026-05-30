840f54673475b29c0b0a5d0b705151388781eba9
Tahani Alhammad <tahani_alhammad@yahoo.com>
2026-05-30 21:55:41 +0200

    update

--- src/app/page.js
+++ src/app/page.js
@@ -7,22 +7,22 @@ import AppButton from "../components/AppButton";
 export default function Home() {
   return (
     <>
-      <HeroSection title="Professionele Websites , Webapplicaties op Maat">
+      <HeroSection title="لمسة فنية من الشموع والريزن المصنوعة يدويًا">
         <p className="mt-6 text-lg leading-8 text-gray-600">
-          Ben je op zoek naar een betrouwbare webontwikkelaar? Zoek niet verder.
-          Ik creëer op maat gemaakte websites en webapplicaties die
-          gebruiksvriendelijk, snel en goed vindbaar zijn op Google.
-        </p>
+في هدايا نوارة نصنع قطعًا فنية فريدة تجمع بين الجمال والهدوء، من الشموع العطرية إلى أعمال الريزن المميزة، وكل قطعة تُصنع يدويًا بحب واهتمام بالتفاصيل.
+       </p>
+
+       
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <AppButton
-            href="https://calendly.com/tahaninawebdeveloper/website-consultatie"
+            href="/contact"
             variant="fill"
           >
-            Afspraak maken
+           اطلب تصميم مخصص
           </AppButton>
 
-          <AppButton href="#" variant="outline">
-            Portfolio bekijken <span aria-hidden="true">→</span>
+          <AppButton href="/service" variant="outline">
+           تصفّح المنتجات<span aria-hidden="true">→</span>
           </AppButton>
         </div>
       </HeroSection>
@@ -32,102 +32,7 @@ export default function Home() {
           <FeaturesSection />
 
           <TopProjects />
-
-          <Image
-            className="dark:invert"
-            src="/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
-
-          <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-            <li className="mb-2 tracking-[-.01em]">
-              Get started by editing{" "}
-              <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-                src/app/page.js
-              </code>
-              .
-            </li>
-            <li className="tracking-[-.01em]">
-              Save and see your changes instantly.
-            </li>
-          </ol>
-
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <a
-              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className="dark:invert"
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
-            <a
-              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Read our docs
-            </a>
-          </div>
         </main>
-        {/* <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
-        </footer> */}
       </div>
     </>
   );
