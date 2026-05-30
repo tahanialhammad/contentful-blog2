c6428ffc9459020ecba9c010aca9f2cc136b8210
Tahani Alhammad <tahani_alhammad@yahoo.com>
2026-05-30 21:12:14 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,24 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     //Contentful host de afbeeldingen extern, dus Next.js moet dat domein vertrouwen.
+//      images: {
+//     domains: ['images.ctfassets.net'],
+//   },
+// };
+
+// export default nextConfig;
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //Contentful host de afbeeldingen extern, dus Next.js moet dat domein vertrouwen.
-     images: {
-    domains: ['images.ctfassets.net'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+      },
+    ],
   },
 };
 
